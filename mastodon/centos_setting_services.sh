3e64fc78c8424a33ac57186f84ea1cc9183f6e43
raku1414 <mokosu@vivaldi.net>
2017-09-28 11:15:52 +0900

    did

--- mastodon/centos_setting_services.sh
+++ mastodon/centos_setting_services.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-yum groupinstall "Development Tools"
-yum install -y kernel-devel kernel-headers libffi-devel gdbm-devel gdbm libyaml-devel readline-devel ncurse-devel libxml2-devel libxslt-devel pkgconfig autoconf bison bison-devel protobuf protobuf-compiler protobuf-devel libidn-devel libicu-devel openssl-devel readline-devel zlib-devel gcc gcc-c++ bzip2 git ImageMagick ffmpeg redis postgresql-contrib postgresql-server postgresql postgresql-devel
 systemctl start redis && sudo systemctl enable $_
 postgresql-setup initdb
 systemctl start postgresql && systemctl enable $_
@@ -8,6 +6,3 @@ sudo su - postgres -c 'psql -c "CREATE USER mastodon CREATEDB;"'
 sed -i.org '/shared_preload_libraries/ s/^#//' /var/lib/pgsql/data/postgresql.conf
 sed -i "/shared_preload_libraries/ s/''/'pg_stat_statements'/" /var/lib/pgsql/data/postgresql.conf
 sed -i "/shared_preload_libraries/a pg_stat_statements.track = all" /var/lib/pgsql/data/postgresql.conf
-
-systemctl restart postgresql
-
---
+++ mastodon/inst_dep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+. /etc/os-release
+
+if [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then
+	sudo apt -y install imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git-core g++ libprotobuf-dev protobuf-compiler pkg-config  gcc autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev nginx redis-server redis-tools postgresql postgresql-contrib libidn11-dev libicu-dev
+elif [ "$ID" = "centos" ] || [ "$ID" = "fedora" ]; then
+	yum groupinstall "Development Tools"
+	yum install -y kernel-devel kernel-headers libffi-devel gdbm-devel gdbm libyaml-devel readline-devel ncurse-devel libxml2-devel libxslt-devel pkgconfig autoconf bison bison-devel protobuf protobuf-compiler protobuf-devel libidn-devel libicu-devel openssl-devel readline-devel zlib-devel gcc gcc-c++ bzip2 git ImageMagick ffmpeg redis postgresql-contrib postgresql-server postgresql postgresql-devel
+elif [ "$ID" = "arch" ]; then
+	sudo pacman -S libjpeg-turbo libpng libtiff libpqxx libxslt git gcc protobuf pkg-config autoconf bison openssl libyaml readline zlib ncurses libffi gdbm nginx redis postgresql libidn icu
+fi
