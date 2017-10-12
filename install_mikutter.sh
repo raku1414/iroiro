50be053e28df219f9d8cd7798e09fe008501df0c
raku1414 <mokosu@vivaldi.net>
2017-10-13 03:30:48 +0900

    did

--- install_mikutter.sh
+++ install_mikutter.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 current=$(pwd)
+
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 cd ~/.rbenv && src/configure && make -C src
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-cat << 'EOF' >> ~/.zshrc
+cat << "EOF" >> ~/.$(echo $0)rc
 export PATH=$HOME/.rbenv/bin:$PATH
 eval "$(rbenv init -)"
 EOF
 
-source ~/.zshrc
+source ~/.$(echo $0)rc
 rbenv install 2.4.2
 rbenv global 2.4.2
 
@@ -22,9 +23,8 @@ cd mikutter && gem install bundler
 cat << EOF > ~/.local/share/applications/mikutter.desktop
 [Desktop Entry]
 Name=Mikutter
-Exec=$(which ruby) $(pwd)/mikutter.rb
+Exec=$(which ruby) $(pwd)/mikutter/mikutter.rb
 Terminal=false
 Type=Application
-Icon=$(pwd)/core/skin/data/icon.png
+Icon=$(pwd)/mikutter/core/skin/data/icon.png
 EOF
-
